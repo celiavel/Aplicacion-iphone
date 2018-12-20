953d3da94eec9d86887db2ee8cda547a4b21163c
celiavel <44905688+celiavel@users.noreply.github.com>
2018-12-20 20:32:37 +0100

    Add files via upload

---
+++ Aplicacion iphone-s/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aplicacion iphone-s
+//
+//  Created by Pacopb on 09/11/2018.
+//  Copyright © 2018 Pacopb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Aplicacion iphone-sTests/Aplicacion_iphone_sTests.swift
@@ -0,0 +1,34 @@
+//
+//  Aplicacion_iphone_sTests.swift
+//  Aplicacion iphone-sTests
+//
+//  Created by Pacopb on 09/11/2018.
+//  Copyright © 2018 Pacopb. All rights reserved.
+//
+
+import XCTest
+@testable import Aplicacion_iphone_s
+
+class Aplicacion_iphone_sTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
